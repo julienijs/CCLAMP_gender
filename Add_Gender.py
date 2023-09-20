776a82115abd8b8cbe1ade74c12f2650df0d927c
Julie <julienijs15@gmail.com>
2023-09-20 17:24:43 +0200

    update code

--- Add_Gender.py
+++ Add_Gender.py
@@ -4,10 +4,24 @@ import pandas as pd
 file_path = 'C:/CCLAMP_gender/C-CLAMP_metadata.txt'
 
 # Define the column names
-columns = ['File', 'Year', 'Title', 'Author', 'DOB', 'POB', 'DOD', 'POD', 'Link']  # Replace with actual column names
+columns = ['File', 'Year', 'Title', 'Author', 'DOB', 'POB', 'DOD', 'POD', 'Link']  # Add column names
 
-# Read the TSV file into a DataFrame
+# Read the txt file into a DataFrame
 df = pd.read_csv(file_path, sep='\t', header=None, names=columns)
+
+
+# Define a function to determine gender based on the 'Author' column
+def determine_gender(author):
+    if author == 'NA':
+        return 'NA'
+    elif ';' in author:
+        return 'Mixed'
+    else:
+        return 'X'
+
+
+# Apply the function to create the 'Gender' column
+df['Gender'] = df['Author'].apply(determine_gender)
 
 # Print the DataFrame
 print(df)
