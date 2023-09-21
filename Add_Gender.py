7297fbf2c366b79dc9db7704b9c258180cd3cb14
Julie <julienijs15@gmail.com>
2023-09-21 11:14:15 +0200

    update code

--- Add_Gender.py
+++ Add_Gender.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from gender_from_name.detector import get_gender
 
 # Define the file path
 file_path = 'C:/CCLAMP_gender/C-CLAMP_metadata.txt'
@@ -12,12 +13,18 @@ df = pd.read_csv(file_path, sep='\t', header=None, names=columns)
 
 # Define a function to determine gender based on the 'Author' column
 def determine_gender(author):
-    if author == 'NA':
-        return 'NA'
-    elif ';' in author:
-        return 'Mixed'
+    if pd.notna(author):
+        if ';' in author:
+            print(author + ": mixed")
+            return 'mixed'
+        else:
+            first_name = author.split()[0]
+            gender = get_gender(first_name)
+            if gender is not None:
+                print(author + ", " + first_name + ": " + gender)
+                return gender
     else:
-        return 'X'
+        return 'NA'
 
 
 # Apply the function to create the 'Gender' column
@@ -25,3 +32,9 @@ df['Gender'] = df['Author'].apply(determine_gender)
 
 # Print the DataFrame
 print(df)
+
+# Define the file path for the output CSV file
+output_file_path = 'C:/CCLAMP_gender/C-CLAMP_metadata_gender.csv'
+
+# Write the DataFrame to a CSV file
+df.to_csv(output_file_path, index=False)
